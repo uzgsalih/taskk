27ba39715696f5542a9352703bac618ddfc8874f
bsuzg <bsuzgas31@gmail.com>
2025-05-15 23:22:34 +0200

    updated

--- ex1.py
+++ ex1.py
@@ -2,3 +2,9 @@ def greet():
     print("Hello from example1!")
 
 greet()
+
+def sumOfNumbers(a,b):
+    return a+b
+
+
+print(sumOfNumbers(a,b))
--- ex2.py
+++ ex2.py
@@ -3,3 +3,7 @@ def add(a, b):
 
 print("Sum of 3 and 5 is:", add(3, 5))
 
+def multi(a,b):
+    return a * b
+
+print(multi(x,y))
