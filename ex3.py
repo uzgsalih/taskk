c95676e3f37bd2c45b78d021e1caf7891d87e2c5
uzgsalih <bsuzgas31@gmail.com>
2025-05-15 23:23:40 +0200

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -2,3 +2,11 @@ def square(n):
     return n * n
 
 print("Square of 4 is:", square(4))
+
+def divide(a, b):
+    if b == 0:
+        return "Can't divide to 0"
+    return a / b
+
+print(divide(8, 4)) 
+print(divide(28, 7))
